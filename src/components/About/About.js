82f79c4e1a90777d5f13ccddc0107a911fdfc50c
Rahul Sood <w0510619@campus.nscc.ca>
2026-04-05 19:57:26 -0300

    Some important additions

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -35,7 +35,7 @@ const About = () => {
 
       <div className='about__contact center'>
         {resume && (
-          <a href={resume}>
+          <a href={resume} target='_blank' rel='noreferrer'>
             <span type='button' className='btn btn--outline'>
               Resume
             </span>
